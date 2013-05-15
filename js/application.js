e30d260d4cab4af9a7344cd10e0d1d5e276ec2fb
Pawis Chairoj <pawis.joe@gmail.com>
2013-05-16 00:04:41 +0700

    update 16/05/13 12.04

--- js/application.js
+++ js/application.js
@@ -10,12 +10,45 @@ $(document).ready(function() {
 		 });
 	});
 	
+	$('#round-shaped-rice').click(function() {
+		$('#submit-kind').val('round-shaped-rice');
+		$('#submit-kind-text').text('Round Shaped Rice');
+	});
+
+	$('#sticky-rice').click(function() {
+		$('#submit-kind').val('sticky-rice');
+		$('#submit-kind-text').text('Sticky Rice');
+	});
+
+	$('#japanese-rice').click(function() {
+		$('#submit-kind').val('japanese-rice');
+		$('#submit-kind-text').text('Japanese Rice');
+	});
+
+	$('#wheat').click(function() {
+		$('#submit-kind').val('wheat');
+		$('#submit-kind-text').text('Wheat');
+	});
+
    	$('#area-select').select2({
-    	placeholder: "Select a Province",
-    	width: 300
+    	placeholder: "Select a Province"
 	}); 
+
+   	$('#area-select').change(function() {
+   		$('#submit-area').val($('#area-select').val());
+   		$('#submit-area-text').text($('#area-select').val());
+   	})
 
 	$('#area i').bind('click', function() {
 		$('#area-select').select2('open');
+	});
+
+	$('#submit-button').click(function() {
+		if($('#submit-kind').val() == "" || $('#submit-area').val() == "") {
+			return false;
+		}
+		else { 
+			$('#riceform').submit();
+		}
 	});
 });
